9bc9023cf7dfa56c34e5589beb8378f6a2026ad5
Janos Pasztor <janos@pasztor.at>
2020-09-15 08:25:22 +0200

    Fixed PDF print issues

--- pdf-theme-handler.py
+++ pdf-theme-handler.py
@@ -3,6 +3,58 @@ from bs4 import BeautifulSoup
 
 def get_stylesheet() -> str:
     return """
+    h1, h2, h3 {
+        string-set: chapter content();
+    }
+    
+    .md-container {
+        display: block;
+        padding-top: 0;
+    }
+    
+    .md-main {
+        display: block;
+        height: inherit;
+    }
+    
+    .md-main__inner {
+        height: inherit;
+        padding-top: 0;
+    }
+    
+    .md-typeset .codehilitetable .linenos {
+        display: none;
+    }
+    
+    .md-typeset .footnote-ref {
+        display: inline-block;
+    }
+    
+    .md-typeset a.footnote-backref {
+        transform: translateX(0);
+        opacity: 1;
+    }
+    .md-typeset .admonition {
+        display: block;
+        border-top: .1rem solid rgba(0,0,0,.07);
+        border-right: .1rem solid rgba(0,0,0,.07);
+        border-bottom: .1rem solid rgba(0,0,0,.07);
+        page-break-inside: avoid;
+    }
+    
+    .md-typeset a::after {
+        color: inherit;
+        content: none;
+    }
+    
+    .md-typeset table:not([class]) th {
+        min-width: 0;
+    }
+    
+    .md-typeset table {
+        border: .1rem solid rgba(0,0,0,.07);
+    }
+    
     body > .container {
         margin-top: -100px;
     }
@@ -21,6 +73,7 @@ def get_stylesheet() -> str:
     
     img {
         max-width:100%;
+        image-resolution: 300dpi;
     }
     ul li img, ol li img {
         width:80% !important;
